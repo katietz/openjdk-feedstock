52924457be7cad449c3d6da5bb2dce31db2602d0
Ray Donnelly <mingw.android@gmail.com>
2017-11-25 01:59:52 +0000

    Some more fixes

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,18 +6,15 @@ fi
 
 chmod +x bin/*
 chmod +x jre/bin/*
-[[ -d $PREFIX/bin ]] || mkdir $PREFIX/bin
-[[ -d $PREFIX/lib ]] || mkdir $PREFIX/lib
-[[ -d $PREFIX/include ]] || mkdir $PREFIX/include
-mv bin/* $PREFIX/bin/
-mv lib/* $PREFIX/lib
-mv include/* $PREFIX/include
+[[ -d "${PREFIX}"/bin ]] || mkdir "${PREFIX}"/bin
+[[ -d "${PREFIX}"/lib ]] || mkdir "${PREFIX}"/lib
+[[ -d "${PREFIX}"/include ]] || mkdir "${PREFIX}"/include
 
 if [[ -e jre/lib/jspawnhelper ]]; then
     chmod +x jre/lib/jspawnhelper
 fi
 
-if [[ $(uname -m) == x86_64 ]]; then
+if [[ ${ARCH} == 64 ]]; then
   _libarch=amd64
 else
   _libarch=i386
@@ -32,22 +29,26 @@ if [[ $(uname) == Linux ]]; then
 
     # include dejavu fonts to allow java to work even on minimal cloud images where these fonts are missing
     # (thanks to @chapmanb)
-    pushd jre/lib/fonts
-        mv dejavu-fonts-ttf-*/ttf/* .
-        rm -rf dejavu-fonts-ttf-*
-    popd
+  pushd jre/lib/fonts
+    mv ttf/* ../
+    rm -rf ttf
+  popd
 fi
 
-mv jre $PREFIX/
+mv jre "${PREFIX}"/
 if [[ -f src.zip ]]; then
-  mv src.zip $PREFIX/jre/
+  mv src.zip "${PREFIX}"/jre/
 fi
+
+mv bin/* "${PREFIX}"/bin/
+mv lib/* "${PREFIX}"/lib/
+mv include/* "${PREFIX}"/include/
 
 # ensure that JAVA_HOME is set correctly
-mkdir -p $PREFIX/etc/conda/activate.d
-mkdir -p $PREFIX/etc/conda/deactivate.d
-cp $RECIPE_DIR/scripts/activate.sh $PREFIX/etc/conda/activate.d/java_home.sh
-cp $RECIPE_DIR/scripts/deactivate.sh $PREFIX/etc/conda/deactivate.d/java_home.sh
+mkdir -p "${PREFIX}"/etc/conda/activate.d
+mkdir -p "${PREFIX}"/etc/conda/deactivate.d
+cp "${RECIPE_DIR}"/scripts/activate.sh "${PREFIX}"/etc/conda/activate.d/java_home.sh
+cp "${RECIPE_DIR}"/scripts/deactivate.sh "${PREFIX}"/etc/conda/deactivate.d/java_home.sh
 
 if [[ $(uname) == Darwin ]]; then
   popd
