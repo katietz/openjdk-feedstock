547b52823cd3e61aa129a70537a3d5ff249358c7
Forrest Waters <fwaters@anaconda.com>
2020-02-11 13:20:23 -0600

    switch to single bracket to be posix compliant

--- recipe/scripts/activate.sh
+++ recipe/scripts/activate.sh
@@ -3,10 +3,10 @@ export JAVA_HOME="${CONDA_PREFIX}"
 
 export CONDA_BACKUP_JAVA_LD_LIBRARY_PATH="${JAVA_LD_LIBRARY_PATH}"
 
-if [[ $(uname) == Darwin ]]; then
+if [ $(uname) == Darwin ]; then
   export JAVA_LD_LIBRARY_PATH="${JAVA_HOME}"/jre/lib/server
 else
-  if [[ $(uname -m) == x86_64 ]]; then
+  if [ $(uname -m) == x86_64 ]; then
     export JAVA_LD_LIBRARY_PATH="${JAVA_HOME}"/jre/lib/amd64/server
   else
     export JAVA_LD_LIBRARY_PATH="${JAVA_HOME}"/jre/lib/i386/server
